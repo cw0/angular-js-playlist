3415717a109caf40eefcb4524776535298c02f8b
cw0 <element4emerica@gmail.com>
2016-12-15 00:52:00 -0500

    caught up to video 15. learned ng-submit

--- app/app.js
+++ app/app.js
@@ -9,24 +9,42 @@ myApp.run(function () {
 });
 
 myApp.controller('AppController', ['$scope', function ($scope) {
+
+  $scope.addFlowmie = function (flowmie) {
+    flowmie.available = true;
+    $scope.flowmies.push(flowmie)
+
+    $scope.newFlowmie = {};
+  }
+
+  $scope.removeFlowmie = function (flowmie) {
+    var removedFlowmie = $scope.flowmies.indexOf(flowmie);
+
+    $scope.flomies.splice(removedFlowmie, 1);
+  }
+
   $scope.flowmies = [
     {
       name : 'Chris',
       prop : 'Staff',
       money : 10,
-      available : true
+      available : true,
+      belt : 'green'
     },
     {
       name : 'Montana',
       prop : 'Hoop',
       money : 20,
-      available : false
+      available : true,
+      belt : 'red'
     },
     {
       name : 'Sam',
       prop : 'Poi',
       money : 5,
-      available : true
+      available : true,
+      belt : 'blue'
     }
   ];
+
 }]);
